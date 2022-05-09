5434a09f16f8afcbadd3f7660abe02063806ca9b
Aleksey <alexeyn525@gmail.com>
2022-05-09 20:30:04 +0300

    add adaptive header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+//Бургер-меню-----------------------------------------------
+let burger = document.querySelector('.menu__burger');
+let burgerMenu = document.querySelector('.burger__menu')
+burger.addEventListener('click', toggleBurgerClass);
+function toggleBurgerClass() 
+{
+   burger.classList.toggle('burger--active');
+   burgerMenu.classList.toggle('menu--active');
+}
