12b95a1fe52176f1d9d3ce39566633daba1a90c0
Aleksey <alexeyn525@gmail.com>
2022-05-15 19:07:42 +0300

    add js code for burger menu add scroll animation

--- js/main.js
+++ js/main.js
@@ -7,4 +7,34 @@ function toggleBurgerClass()
 {
    burger.classList.toggle('burger--active');
    burgerMenu.classList.toggle('menu--active');
+
+   if (burger.classList.contains('burger--active'))
+   {
+       document.body.style.overflow = "hidden";
+   }
+   else
+   {
+       document.body.style.overflow = "visible"; 
+   }
 }
+const hrefBurger = document.querySelectorAll('.burger__href');
+for (let i = 0; i < hrefBurger.length; i++)
+{
+    hrefBurger[i].addEventListener('click', removeState);
+}
+function removeState() {
+    $('body').css('overflow', 'visible');
+    burger.classList.remove('burger--active');
+    burgerMenu.classList.remove('menu--active');
+}
+//--плавный скролл------------------------------------------
+$(document).ready(function(){
+   $("a[href*='#']").on("click", function(e){
+     var anchor = $(this);
+     $('html, body').stop().animate({
+       scrollTop: $(anchor.attr('href')).offset().top
+     }, 777);
+     e.preventDefault();
+     return false;
+   });
+ });
