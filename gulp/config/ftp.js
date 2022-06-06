312539dc0d2ed41f7f57ae722ee3641e0abf1e3d
Aleksey <alexeyn525@gmail.com>
2022-06-06 22:15:42 +0300

    added taskrunner and his config

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+  host: 'w97711cp.beget.tech', // Адрес FTP сервера
+  user: 'w97711cp_1', // Имя пользователя
+  password: 'Alexey1999!', // Пароль
+  parallel: 20 // Кол-во одновременных потоков
+}
